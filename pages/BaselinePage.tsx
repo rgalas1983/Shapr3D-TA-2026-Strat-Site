89b3b3831bbef84719020f593382ddb8a101d263
rgalas1983 <robert.galas@shapr3d.com>
2026-01-19 16:07:08 +0100

    Update benchmark names to include values

--- pages/BaselinePage.tsx
+++ pages/BaselinePage.tsx
@@ -16,13 +16,13 @@ const BaselinePage: React.FC = () => {
   ];
 
   const benchmarksEng = [
-    { name: '2025 S3D', value: 94 },
-    { name: 'EU SaaS Avg', value: 85, isBenchmark: true }
+    { name: '2025 S3D = 94', value: 94 },
+    { name: 'EU SaaS Avg = 85', value: 85, isBenchmark: true }
   ];
 
   const benchmarksSales = [
-    { name: '2025 S3D', value: 65 },
-    { name: 'Global AE Avg', value: 75, isBenchmark: true }
+    { name: '2025 S3D = 65', value: 65 },
+    { name: 'Global AE Avg = 75', value: 75, isBenchmark: true }
   ];
 
   return (
